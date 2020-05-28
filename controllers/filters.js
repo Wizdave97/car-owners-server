b82d6d7d5806666e5059fe1ade135c21e0368e43
David Salami <wizdave97@gmail.com>
2020-05-28 16:01:13 +0100

    feat: Created filters REST Endpoint
    This REST Endpoint fetches list of filters from the database

---
+++ controllers/filters.js
@@ -0,0 +1,41 @@
+const models = require('../models');
+const { validationResult } = require('express-validator');
+
+module.exports = {
+    getFilters(req, res)
+    {
+        try
+        {
+            const errors = validationResult(req)
+            if(!errors.isEmpty())
+            {
+                res.setHeader('Content-Type', 'application/json')
+                res.status(400).send({ status: 400, error: errors.array() })
+                return;
+            }
+            const limit = 10;
+            const page = +req.query.page;
+            const offset = (page - 1) * limit; 
+            models.Filters.findAndCountAll({
+                limit,
+                offset
+            }).then(({rows, count}) => {
+                const next = (count - offset) > 0 && (count > limit)  ? page + 1 : null;
+                const prev = page > 1 ? page - 1 : null;
+                const nextPage = process.env.NODE_ENV === 'development' ?
+                (next ? `http://localhost:3000/filters?page=${next}` : null) : (next ? `https:// /filters?page=${next}`: null)
+                const prevPage =  process.env.NODE_ENV === 'development' ?
+                (prev ? `http://localhost:3000/filters?page=${prev}` : null) : (prev ? `https:// /filters?page=${prev}`: null)
+                res.status(200).send({ status: 200, data:[...rows], nextPage, prevPage });
+                return;
+            }).catch((err)=>{
+                res.status(500).send({ status: 500, error: "An error occured while fetching results" })
+                return;
+            })
+        }
+        catch (err)
+        {
+            res.status(500).send({ status:500, error:"An unknown error occured" })
+        }
+    }
+}
--- routes/filters.js
+++ routes/filters.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const filters = require('../controllers/filters');
+const { query } = require('express-validator');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', [
+  query("page", "The page number is required, must be a number").isNumeric().trim().escape()
+], filters.getFilters);
 
 module.exports = router;
