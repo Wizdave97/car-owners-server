f6916a3b7d3aa61c82b15d08bd0560595cd51a47
David Salami <wizdave97@gmail.com>
2020-05-29 13:01:40 +0100

    fix: corrected typograhical error in response from people route

--- controllers/people.js
+++ controllers/people.js
@@ -27,7 +27,7 @@ module.exports = {
                 const next = (count - offset) > 0 && (count > limit)  ? page + 1 : null;
                 const prev = page > 1 ? page - 1 : null;
                 const nextPage = process.env.NODE_ENV === 'development' ?
-                (next ? `http://localhost:3000/people?page=${next}` : null) : (next ? `https://immense-ravine-06184.herokuapp.com/filters?page=${next}`: null)
+                (next ? `http://localhost:3000/people?page=${next}` : null) : (next ? `https://immense-ravine-06184.herokuapp.com/people?page=${next}`: null)
                 const prevPage =  process.env.NODE_ENV === 'development' ?
                 (prev ? `http://localhost:3000/people?page=${prev}` : null) : (prev ? `https://immense-ravine-06184.herokuapp.com/people?page=${prev}`: null)
                 const filteredRows = filter(rows, req.body)
